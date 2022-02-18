004494760ec5b2b321a42ca3256cbe32075d5111
teaflex <teaflex.dev@hotmail.com>
2022-02-18 22:22:23 +0100

    folder based models

--- api/models/User.py
+++ api/models/User.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    pass
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    hashpass = models.CharField(max_length=50)
+    
